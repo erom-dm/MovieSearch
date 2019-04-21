094b0b1c36b330cd6a73ea4630a5151a8f19d322
erom-dm <swiftyerom@gmail.com>
2019-04-21 14:49:37 +0300

    api data in comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import FetchData from './util/FetchData';
 
-const App = () => (
-  <div> hey </div>
-);
+class App extends Component {
+  state = {
+    movies: {},
+  };
+
+  componentDidMount() {
+    FetchData.get();
+  }
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div> hey </div>
+    );
+  }
+}
 
 export default App;
+// https://api.themoviedb.org/3/movie/76341?api_key={api_key}
+// 5874acfd11651a28c55771624f7021f4
+
+// for images use: https://image.tmdb.org/t/p/w200 + ${path} (w - width of image in pixels)
+
+// seach functionality: https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+// with general movie info from 'results' field, we can query following to get full info: https://api.themoviedb.org/3/movie/343611?api_key={api_key}
